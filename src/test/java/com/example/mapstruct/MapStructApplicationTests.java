b4309c7390292e1c4fe958ea7490582a46b69731
yeongun <gjduddns@gmail.com>
2023-01-11 14:08:34 +0900

    git init

---
+++ src/test/java/com/example/mapstruct/MapStructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapStructApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
